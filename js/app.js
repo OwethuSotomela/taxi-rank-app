c55f637aebf1d5d9ed4615c8da2b578ef5b3746b
OwethuSotomela <owethusotomela@gmail.com>
2022-05-18 16:27:23 +0200

    Restructurring and calculating total

--- js/app.js
+++ js/app.js
@@ -14,49 +14,8 @@ document.addEventListener('alpine:init', () => {
                     fare: 22,
                     trips: 0,
                     taxis: 4,
+                    overallTotal: 0
 
-                    taxiFull: 'Mini taxi full & can leave the rank',
-                    taxiNotFull: 'Taxi cannot leave the rank unless full',
-                    invalidAction: 'Invalid action',
-                    feedback: '',
-
-                    getTotalFare() {
-                        return Number(this.queue) * Number(this.fare)
-                    },
-                    queueInLine() {
-                        this.queue++
-                        if (this.queue >= 7) {
-
-                            setTimeout(() => {
-                                this.feedback = this.taxiFull
-                            }, 0000);
-                        }
-                    },
-                    leaveQueue() {
-                        if (this.queue >= 1) {
-                            this.queue--
-                        } else {
-
-                            setTimeout(() => {
-                                this.feedback = this.invalidAction
-                            }, 0000);
-                        }
-                    },
-                    leave() {
-
-                        if (this.queue < 7) {
-
-                            setTimeout(() => {
-                                this.feedback = this.taxiNotFull
-                            }, 0000)
-
-                        } else {
-                            this.trips++
-                            this.taxis--
-                            this.queue -= this.limit
-                        }
-
-                    }
                 },
                 {
                     destination: 'Parow',
@@ -65,49 +24,7 @@ document.addEventListener('alpine:init', () => {
                     fare: 18,
                     trips: 0,
                     taxis: 4,
-
-                    taxiFull: 'Mini taxi full & can leave the rank',
-                    taxiNotFull: 'Taxi cannot leave the rank unless full',
-                    invalidAction: 'Invalid action',
-                    feedback: '',
-
-                    getTotalFare() {
-                        return Number(this.queue) * Number(this.fare)
-                    },
-                    queueInLine() {
-                        this.queue++
-                        if (this.queue >= 7) {
-
-                            setTimeout(() => {
-                                this.feedback = this.taxiFull
-                            }, 0000)
-                        }
-                    },
-                    leaveQueue() {
-
-                        if (this.queue >= 1) {
-                            this.queue--
-                        } else {
-
-                            setTimeout(() => {
-                                this.feedback = this.invalidAction
-                            }, 0000)
-                        }
-                    },
-                    leave() {
-
-                        if (this.queue < 7) {
-
-                            setTimeout(() => {
-                                this.feedback = this.taxiNotFull
-                            }, 0000)
-                        } else {
-                            this.trips++
-                            this.taxis--
-                            this.queue -= this.limit
-                        }
-                        
-                    }
+                    overallTotal: 0
 
                 },
                 {
@@ -117,49 +34,73 @@ document.addEventListener('alpine:init', () => {
                     fare: 12,
                     trips: 0,
                     taxis: 4,
+                    overallTotal: 0
 
-                    taxiFull: 'Mini taxi full & can leave the rank',
-                    taxiNotFull: 'Taxi cannot leave the rank unless full',
-                    invalidAction: 'Invalid action',
-                    feedback: '',
-
-                    getTotalFare() {
-                        return Number(this.queue) * Number(this.fare)
-                    },
-                    queueInLine() {
-                        this.queue++
-                        if (this.queue >= 7) {
-
-                            setTimeout(() => {
-                                this.feedback = this.taxiFull
-                            }, 0000)
-                        }
-                    },
-                    leaveQueue() {
-
-                        if (this.queue >= 1) {
-                            this.queue--
-                        } else {
-                            setTimeout(() => {
-                                this.feedback = this.invalidAction
-                            }, 0000)
-                        }
-                    },
-                    leave() {
-
-                        if (this.queue <= 7) {
-                            setTimeout(() => {
-                                this.feedback = this.taxiNotFull
-                            })
-                        } else {
-                            this.trips++
-                            this.taxis--
-                            this.queue -= this.limit
-                        }
-
-                    }
-                }
+                },
             ],
+            addRoute(stop, fare) {
+
+                ranks.push({
+                    destination: stop,
+                    limit: 7,
+                    queue: 0,
+                    fare: fare,
+                    trips: 0,
+                    taxis: 4,
+                    overallTotal: 0
+                })
+                console.log(stop, fare)
+            },
+
+            queueInLine(destination) {
+
+                destination.queue++
+
+                if (destination.queue >= 7) {
+
+                    setTimeout(() => {
+                        this.feedback = this.taxiFull
+                    }, 0000)
+                }
+
+            },
+            leaveQueue(destination) {
+
+                if (destination.queue >= 1) {
+                    destination.queue--
+                } else {
+                    setTimeout(() => {
+                        this.feedback = this.invalidAction
+                    }, 0000)
+                }
+
+            },
+            leave(destination) {
+
+                if (destination.queue <= 6) {
+                    setTimeout(() => {
+                        this.feedback = this.taxiNotFull
+                    })
+                } else {
+                    destination.trips++
+                    destination.taxis--
+                    destination.queue -= destination.limit
+                    this.getTotalFare(destination)
+                }
+
+            },
+
+            taxiFull: 'Mini taxi full & can leave the rank',
+            taxiNotFull: 'Taxi cannot leave the rank unless full',
+            invalidAction: 'Invalid action',
+            feedback: '',
+
+            getTotalFare(destination) {
+                let newTotal = destination.limit * destination.fare
+                console.log(newTotal)
+                destination.overallTotal += newTotal
+            },
+
         }
     })
 })
